65cc41423009f9cd7dc69604f1a1a01bfcecd6c3
Anna Merkul <amerkul@solvd.com>
2023-10-17 15:30:44 +0300

    Clean controller

--- src/com/solvd/airport/controller/user-controller.ts
+++ src/com/solvd/airport/controller/user-controller.ts
@@ -4,7 +4,6 @@ import { authService } from "../security/auth-service";
 import AuthenticationUserDetails from "../security/auth-user-details";
 import * as bcrypt from 'bcrypt';
 import AuthenticatedUserDto from "../dto/auth-user-dto";
-import UsernameNotFoundException from "../exception/username-not-found-exception";
 import AuthenticationException from "../exception/auth-exception";
 import User from "../model/user";
 import { userService } from "../service/user-service";
@@ -28,10 +27,8 @@ class UserController {
 
     async login(req: Request, res: Response, next: NextFunction) {
         const login: LoginDto = req.body;
-        console.log(login);
         const authUser: AuthenticationUserDetails = await authService.loadUserByUsername(login.username);
         if (authUser) {
-            console.log(await bcrypt.hash(login.password, 10))
             const authentication = await bcrypt.compare(login.password, authUser.password);
             if (authentication) {
                 const token = authUtil.createToken(authUser);
@@ -40,7 +37,7 @@ class UserController {
                 next(new AuthenticationException(401, "Unauthorized"));
             }
         } else {
-            next(new UsernameNotFoundException(404, "Not found"));
+            next(new AuthenticationException(401, "Unauthorized"));
         }
     }
 
