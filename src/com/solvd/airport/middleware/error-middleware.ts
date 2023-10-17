245b36aa0333f52774455306fd4b729a41d3b4f1
Anna Merkul <amerkul@solvd.com>
2023-10-17 15:36:35 +0300

    Add internal exception

--- src/com/solvd/airport/middleware/error-middleware.ts
+++ src/com/solvd/airport/middleware/error-middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import CustomError from "../exception/custom-error";
 
 export function errorMiddleware(error: CustomError, request: Request, response: Response, next: NextFunction) {
-    const status = error.status || 500;
-    const message = error.message || 'Something went wrong';
+    const status = error.status;
+    const message = error.message;
     response.status(status).send({status, message,});
 }
